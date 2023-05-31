f9c05d1213dd9748893797f6f0a23411abd02cb2
xafish <xafish@rambler.ru>
2023-05-31 16:19:07 +0300

    Домашнее задание hw06 JPQL, Spring ORM, DAO на основе Spring ORM + JPA revert

--- hw06/src/main/java/ru/otus/spring/repositories/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.spring.repositories;
-
-import ru.otus.spring.domain.Author;
-
-public interface AuthorRepository {
-    Author getAuthorByNameLastname(String authorName, String authorLastName);
-}
--- hw06/src/main/java/ru/otus/spring/repositories/GenreRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.spring.repositories;
-
-import ru.otus.spring.domain.Genre;
-
-public interface GenreRepository {
-    Genre getGenreByName(String name);
-}
